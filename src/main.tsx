7247e8b0a707904a4588c433aff275bf68434000
Lindsay Morales <lindsaymorales15@gmail.com>
2024-07-27 17:12:44 -0600

    Add web3modal integration for wallets

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { Web3ModalProvider } from './utilities/Web3Modal'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Web3ModalProvider />
   </React.StrictMode>,
 )
---
+++ src/utilities/Web3Modal.tsx
@@ -0,0 +1,41 @@
+import App from '../App.tsx'
+
+import { createWeb3Modal } from "@web3modal/wagmi/react";
+import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
+
+import { WagmiProvider } from 'wagmi'
+import { optimismSepolia } from 'wagmi/chains'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+const projectId = '521a47a7941cb38e29ab641f467b61f1'
+
+const metadata = {
+  name: 'Move&Mint',
+  description: 'Fitness App: Mint your goals',
+  url: 'https://move&mint.app',
+  icons: ['https://move&mint.app/icons']
+}
+
+const chains = [optimismSepolia] as const
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+})
+
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true // Optional - defaults to your Cloud configuration
+})
+
+export function Web3ModalProvider() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
